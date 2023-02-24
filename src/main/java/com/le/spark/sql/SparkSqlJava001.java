56460d0987c7e74c01eb93d8fd1d2f32397edeb5
hubo88 <hubo88@jd.com>
2023-02-24 14:36:18 +0800

    row list 元素类型判断

--- src/main/java/com/le/spark/sql/SparkSqlJava001.java
+++ src/main/java/com/le/spark/sql/SparkSqlJava001.java
@@ -1,13 +1,18 @@
 package com.le.spark.sql;
 
+import java.math.BigDecimal;
+import java.util.List;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SQLContext;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 import org.spark_project.dmg.pmml.DataType;
+import scala.collection.JavaConversions;
+import scala.collection.mutable.WrappedArray;
 
 /**
  * @Description P101
@@ -23,9 +28,26 @@ public class SparkSqlJava001 {
                 .getOrCreate();
 
         //Dataset stuDataset = sparkSession.read().schema("id INT, name STRING, age INT").json("/Users/allen/bigdataapp/spark007/src/resources/input/students.json");
-        Dataset stuDataset = sparkSession.read().json("/Users/allen/bigdataapp/spark007/src/resources/input/students.json");
+        Dataset<Row> stuDataset = sparkSession.read().json("/Users/hubo88/allen_mac/bigdata_app/spark007/src/resources/input/students.json");
 
         stuDataset.printSchema();
+
+        Iterable<Row> rows = stuDataset.collectAsList();
+        // list 字段处理
+        for (Row row : rows) {
+            if (row.getAs("group") instanceof WrappedArray) {
+                WrappedArray arr = (WrappedArray)(row.getAs("group"));
+                JavaConversions.seqAsJavaList(arr).stream().forEach(value -> {
+                    Object wrapValue = value;
+                    System.out.print((new BigDecimal(wrapValue.toString())).intValue());
+                    System.out.print("   ");
+                });
+            } else {
+                System.out.println(row.getAs("group").getClass());
+                System.out.println("else");
+            }
+
+        }
 
         // 条件查询
         stuDataset.select("name")
